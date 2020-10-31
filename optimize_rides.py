a75fa54340537e7a0947f17b084f2a834887807c
Axel <axel.hertsberg@gmail.com>
2020-10-31 22:57:54 +0200

    Add passenger reading and example file

--- optimize_rides.py
+++ optimize_rides.py
@@ -13,8 +13,14 @@ locations.set_destination(2)
 graph = optimizer.OptimizationGraph(locations)
 locations.add_location(7, 62.3, 21.93)
 graph.update_locations(locations)
+passenger_file = pd.read_csv("passengers.csv", sep=";")
+for index in passenger_file.index:
+    passenger = optimizer.Passenger(
+                          passenger_file["passenger_id"][index], 
+                          passenger_file["start_id"][index])
+    locations.add_passenger(passenger)
 
 print(graph.get_edges())
-
+print(locations.get_passengers(1))
 
 
